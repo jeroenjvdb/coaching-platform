2fbef999ad28bb80fd5d103bed927bfce4007248
Jeroen Van den Broeck <jeroen.vandenbroeck@student.kdg.be>
2016-05-25 19:56:40 +0200

    make changes to lading'

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -1,93 +1,20 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="_token" content="{{ csrf_token() }}">
 
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet'
-          type='text/css'>
-    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
+    <title>@yield('title') | coaching platform | KAZSC</title>
+
+    <link rel="manifest" href="/manifest.json">
+    <meta name="viewport" content="width=device-width">
+    <meta name="mobile-web-app-capable" content="yes">
+    <link rel="icon" sizes="400x400" href="/icon.png">
 
     <!-- Styles -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/resources/css/main.css" rel="stylesheet">
-    <style>
-   /*     body.variation {
-            background: #32323A;
-            font-size: 1.5em;
-        }
 
-        .login-box {
-            width: 100%;
-            max-width: 350px;
-            margin: 150px auto;
-            background: #eaeaec;
-            position: relative;
-            overflow: hidden;
-            -moz-border-radius: 8px;
-            -webkit-border-radius: 8px;
-            border-radius: 8px;
-        }
-
-        .login-box .in {
-            padding: 20px;
-        }
-
-        .login-box .field {
-            width: 100%;
-            float: left;
-            margin: 10px 0 0 0;
-        }
-
-        input[type=text], input[type=password], input[type=email], input[type=url], textarea {
-            padding: 10px;
-            height: 40px;
-            width: 100%;
-            max-width: 100%;
-            background: #fff;
-            border: 1px solid #d6d6d6;
-            -moz-border-radius: 8px;
-            -webkit-border-radius: 8px;
-            border-radius: 8px;
-        }
-
-        .login-box .visual {
-            width: 50%;
-            overflow: hidden;
-            margin: 0 auto 10px;
-        }
-
-        .login-box .field .btn,
-        .login-box .field .dataTables_wrapper .dataTables_paginate a,
-        .dataTables_wrapper .dataTables_paginate .login-box .field a {
-            display: block;
-            width: 100%;
-            text-align: center;
-        }
-
-        .btn, .dataTables_wrapper .dataTables_paginate a {
-            display: inline-block;
-            padding: 15px 20px;
-            //background: #e11b22;
-            color: #fff;
-            border: none;
-            outline: none;
-            font-size: 13px;
-            line-height: 18px;
-            -webkit-font-smoothing: antialiased !important;
-            -moz-border-radius: 8px;
-            -webkit-border-radius: 8px;
-            border-radius: 8px;
-            -moz-transition: background-color, 125ms;
-            -o-transition: background-color, 125ms;
-            -webkit-transition: background-color, 125ms;
-            transition: background-color, 125ms;
-        }
-
-        .groups{
-            color:white;
-        }*/
-    </style>
 
 </head>
 <body class="variation">
